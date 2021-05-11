e36be39e6bd4b48319e88f7e6ad6675b05b5e798
Guvanch <sylapov@itu.edu.tr>
2021-05-11 16:35:48 +0300

    Project is already in remote repo,

--- src/Hello.java
+++ src/Hello.java
@@ -3,5 +3,11 @@ public class Hello {
         System.out.println("Hello");
         //Guvanch
         //Wekilbazar
+
+        //I have already published on Github.com
+        //Now I'm ready to make more commits
+        //but do I need to share again?
+        //Absolutely NO
+        //It's time commit and just push!
     }
 }
